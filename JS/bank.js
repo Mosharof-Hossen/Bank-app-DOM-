f3a78bce97cb4456dd4a0c9c8515b7a1a3b661b9
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2024-06-22 03:01:32 +0600

    all functionality added

--- JS/bank.js
+++ JS/bank.js
@@ -2,11 +2,32 @@ document.getElementById('btn-deposit').addEventListener('click', () => {
     let depositAmount = parseFloat(parseFloat(document.getElementById("deposit-amount").value).toFixed(2));
 
     if (depositAmount) {
-        console.log("ok", depositAmount);
         let totalDeposit = document.getElementById("total-deposit").innerText;
         document.getElementById('total-deposit').innerText = parseFloat(totalDeposit) + depositAmount;
+        let totalBalance = document.getElementById("total-balance").innerText;
+        document.getElementById("total-balance").innerText = parseFloat(totalBalance) + depositAmount;
     }
     else {
-        alert("Enter Number");
+        alert("Enter Amount");
+    }
+})
+
+document.getElementById('btn-withdraw').addEventListener('click', () => {
+    let withdrawAmount = parseFloat(parseFloat(document.getElementById("withdraw-amount").value).toFixed(2));
+
+    if (withdrawAmount) {
+        let totalBalance = document.getElementById("total-balance").innerText;
+        let balanceBalance = parseFloat(totalBalance) - withdrawAmount;
+        if (balanceBalance >= 0) {
+            let totalWithdraw = document.getElementById("total-withdraw").innerText;
+            document.getElementById('total-withdraw').innerText = parseFloat(totalWithdraw) + withdrawAmount;
+            document.getElementById("total-balance").innerText = balanceBalance;
+        }
+        else{
+            alert("Low Balance");
+        }
+    }
+    else {
+        alert("Enter Amount");
     }
 })
